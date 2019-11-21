7b218904727f87ff858a441702cb65b9e113ace0
Tom Banish <tombanish@gmail.com>
2019-11-21 01:19:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,18 +16,17 @@ WIN_COMBINATIONS = [
 ]
 
 
-def won?(board)
-  empty_board = board.all?(" ") || board.all?("")
-	WIN_COMBINATIONS.each do |win_combo|
-	  if win_combo.all? {|index| board[index] == "X"} || win_combo.all? {|index| board[index] == "O"}
-			return win_combo
-		elsif empty_board
-		  return false
-		elsif full?(board) && (!win_combo.all? {|index| board[index] == "X"} || !win_combo.all? {|index| board[index] == "O"})
+def won?(board) 
+	WIN_COMBINATIONS.each do |wincombo| 
+		if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" 
+			return wincombo 
+		elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O" 
+			return wincombo
+		else
 		  return false
 		end
 	end
-end
+end	
 
 
 def full?(board)
