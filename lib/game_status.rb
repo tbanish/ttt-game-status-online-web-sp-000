06051122aed70609c6788c1d46ef516e23b4583b
Tom Banish <tombanish@gmail.com>
2019-11-21 00:50:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,6 +42,10 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
+  elsif !won?(board) && !full?(board)
+    return false
+  elsif won?(board)
+    return false
   end
 end
 
