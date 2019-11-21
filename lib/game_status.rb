ee38730205d4921d121c9f3abbab039bfb8f0e37
Tom Banish <tombanish@gmail.com>
2019-11-21 01:27:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,7 +57,16 @@ def over?(board)
 end
 
 
-
+def winner(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X" 
+			return "X"
+		elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O" 
+			return "O"
+		end
+	end
+	nil
+end
     
   
   
